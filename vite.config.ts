e29ed6e06ef97f1c563960f06c4a0e0f1d9bb5e6
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-02 15:32:14 +0000

    build: introduce Dockerfile and docker-compose
    [wip]: got docker stuff working
    
    [wip] do not use yarn
    
    [wip]: delete schema.sql

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
     plugins: [sveltekit()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
+    },
+    server: {
+        // for use within Docker
+        host: true,
+        port: 5173,
+        strictPort: true
     }
 });
