6d21da01d3ec0da5a4f39caf17aa49503e9f8e61
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-03 15:07:35 +0000

    feat(debug): add an option to delete the current participantId

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<script>
+    export let data;
+</script>
+
 <header>
     <h1>Debug page</h1>
 </header>
+
+<h2>Welcome, {data.participantId ? data.participantId : 'guest'}!</h2>
 
 <nav>
     <p>Jump quickly to&hellip;</p>
@@ -10,3 +16,9 @@
         <li><a href="/questionnaire">Questionnaire</a></li>
     </ul>
 </nav>
+
+<hr />
+
+<form method="POST" action="?/debugResetParticipantId">
+    <button type="submit" disabled={!data.participantId}> Clear participant_id cookie </button>
+</form>
