ab16f5074c7f36db5ce9afee44c65babbdc1a9b1
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-03 10:54:41 +0000

    refactor: extract component: ConsentCheckbox

---
+++ src/lib/components/ConsentCheckbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let name: string;
+    export let value: boolean;
+</script>
+
+<div class="consent-checkbox">
+    <label>
+        <input type="checkbox" {name} bind:checked={value} />
+        <slot />
+    </label>
+</div>
+
+<style>
+    .consent-checkbox:hover {
+        background-color: #eee;
+    }
+</style>
