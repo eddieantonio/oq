bdd360e546358aee3a60db2d4bb0d58b138dbccb
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-03 11:05:59 +0000

    change: basic styles for consent checkboxes

--- src/lib/components/ConsentCheckbox.svelte
+++ src/lib/components/ConsentCheckbox.svelte
@@ -3,15 +3,37 @@
     export let value: boolean;
 </script>
 
-<div class="consent-checkbox">
-    <label>
+<label>
+    <div class="box">
         <input type="checkbox" {name} bind:checked={value} />
+    </div>
+    <div class="text">
         <slot />
-    </label>
-</div>
+    </div>
+</label>
 
 <style>
-    .consent-checkbox:hover {
-        background-color: #eee;
+    label {
+        display: flex;
+        flex-flow: row nowrap;
+        gap: 1rem;
+
+        cursor: pointer;
+
+        margin-block-end: 0.5lh;
+    }
+
+    .box {
+        flex: 0 0 auto;
+        display: grid;
+        place-items: center;
+    }
+
+    .text {
+        flex: 1 1 auto;
+    }
+
+    label:hover {
+        background-color: var(--nc-bg-3);
     }
 </style>
