49faa8405dd138716c19170dde6dce7cb003b3f7
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-03 10:42:59 +0000

    feat: add debug page as home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<header>
+    <h1>Debug page</h1>
+</header>
+
+<nav>
+    <p>Jump quickly to&hellip;</p>
+    <ul>
+        <li><a href="/answers">Answers</a></li>
+        <li><a href="/consent">Consent</a></li>
+        <li><a href="/questionnaire">Questionnaire</a></li>
+    </ul>
+</nav>
