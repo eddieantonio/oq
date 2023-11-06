621102deb521c7405641ed5ce0cca6c1b00bf1be
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-06 10:58:43 +0000

    change: table-based likert scale

--- src/lib/components/LikertScale.svelte
+++ src/lib/components/LikertScale.svelte
@@ -1,83 +1,58 @@
 <script lang="ts">
     export let questionId: string;
+
+    function id(num: number) {
+        return `${questionId}-${num}`;
+    }
 </script>
 
 <fieldset>
     <legend><slot /></legend>
 
-    <div class="scale-degrees">
-        <div class="degree">
-            <label for={questionId + '-' + 1}>Complete Beginner</label><input
-                id={questionId + '-' + 1}
-                type="radio"
-                name={questionId}
-                value="1"
-            />
-        </div>
-        <div class="degree">
-            <label for={questionId + '-' + 2} /><input
-                id={questionId + '-' + 2}
-                type="radio"
-                name={questionId}
-                value="2"
-            />
-        </div>
-        <div class="degree">
-            <label for={questionId + '-' + 3} /><input
-                id={questionId + '-' + 3}
-                type="radio"
-                name={questionId}
-                value="3"
-            />
-        </div>
-        <div class="degree">
-            <label for={questionId + '-' + 4}>Intermediate</label><input
-                id={questionId + '-' + 4}
-                type="radio"
-                name={questionId}
-                value="4"
-            />
-        </div>
-        <div class="degree">
-            <label for={questionId + '-' + 5} /><input
-                id={questionId + '-' + 5}
-                type="radio"
-                name={questionId}
-                value="5"
-            />
-        </div>
-        <div class="degree">
-            <label for={questionId + '-' + 6} /><input
-                id={questionId + '-' + 6}
-                type="radio"
-                name={questionId}
-                value="6"
-            />
-        </div>
-        <div class="degree">
-            <label for={questionId + '-' + 7}>Expert</label><input
-                id={questionId + '-' + 7}
-                type="radio"
-                name={questionId}
-                value="7"
-            />
-        </div>
-    </div>
+    <table>
+        <thead>
+            <tr>
+                <th><label for={id(1)}>Complete Beginner</label></th>
+                <th><label for={id(2)} /></th>
+                <th><label for={id(3)} /></th>
+                <th><label for={id(4)}>Intermediate</label></th>
+                <th><label for={id(5)} /></th>
+                <th><label for={id(6)} /></th>
+                <th><label for={id(7)}>Expert</label></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><input id={id(1)} type="radio" name={questionId} value="1" /></td>
+                <td><input id={id(2)} type="radio" name={questionId} value="2" /></td>
+                <td><input id={id(3)} type="radio" name={questionId} value="3" /></td>
+                <td><input id={id(4)} type="radio" name={questionId} value="4" /></td>
+                <td><input id={id(5)} type="radio" name={questionId} value="5" /></td>
+                <td><input id={id(6)} type="radio" name={questionId} value="6" /></td>
+                <td><input id={id(7)} type="radio" name={questionId} value="7" /></td>
+            </tr>
+        </tbody>
+    </table>
 </fieldset>
 
 <style>
-    .scale-degrees {
-        display: flex;
-        flex-flow: row nowrap;
+    th {
+        width: calc(100% / 7);
+        font-size: small;
+        vertical-align: bottom;
     }
-    .degree {
-        display: flex;
-        flex-flow: column nowrap;
-        flex: 1;
-    }
-    .degree label {
-        flex: 1;
-        font-size: smaller;
+    th,
+    td {
         text-align: center;
+        border: 0;
+        padding-inline: 0;
+    }
+    label {
+        display: inline-block;
+        width: 100%;
+    }
+    input {
+        cursor: pointer;
+        width: 100%;
     }
 </style>
