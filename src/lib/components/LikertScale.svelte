f263bb0152f3180b207a9dbaa604b33c62c1f495
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-06 10:40:43 +0000

    change: better (???) likert scale input

--- src/lib/components/LikertScale.svelte
+++ src/lib/components/LikertScale.svelte
@@ -2,14 +2,82 @@
     export let questionId: string;
 </script>
 
-<div class="input-group">
-    <label for={questionId}><slot /> (1=complete beginner, 7=expert)</label><input
-        type="number"
-        id={questionId}
-        name={questionId}
-        min="1"
-        max="7"
-        step="1"
-        value="1"
-    />
-</div>
+<fieldset>
+    <legend><slot /></legend>
+
+    <div class="scale-degrees">
+        <div class="degree">
+            <label for={questionId + '-' + 1}>Complete Beginner</label><input
+                id={questionId + '-' + 1}
+                type="radio"
+                name={questionId}
+                value="1"
+            />
+        </div>
+        <div class="degree">
+            <label for={questionId + '-' + 2} /><input
+                id={questionId + '-' + 2}
+                type="radio"
+                name={questionId}
+                value="2"
+            />
+        </div>
+        <div class="degree">
+            <label for={questionId + '-' + 3} /><input
+                id={questionId + '-' + 3}
+                type="radio"
+                name={questionId}
+                value="3"
+            />
+        </div>
+        <div class="degree">
+            <label for={questionId + '-' + 4}>Intermediate</label><input
+                id={questionId + '-' + 4}
+                type="radio"
+                name={questionId}
+                value="4"
+            />
+        </div>
+        <div class="degree">
+            <label for={questionId + '-' + 5} /><input
+                id={questionId + '-' + 5}
+                type="radio"
+                name={questionId}
+                value="5"
+            />
+        </div>
+        <div class="degree">
+            <label for={questionId + '-' + 6} /><input
+                id={questionId + '-' + 6}
+                type="radio"
+                name={questionId}
+                value="6"
+            />
+        </div>
+        <div class="degree">
+            <label for={questionId + '-' + 7}>Expert</label><input
+                id={questionId + '-' + 7}
+                type="radio"
+                name={questionId}
+                value="7"
+            />
+        </div>
+    </div>
+</fieldset>
+
+<style>
+    .scale-degrees {
+        display: flex;
+        flex-flow: row nowrap;
+    }
+    .degree {
+        display: flex;
+        flex-flow: column nowrap;
+        flex: 1;
+    }
+    .degree label {
+        flex: 1;
+        font-size: smaller;
+        text-align: center;
+    }
+</style>
--- src/routes/questionnaire/+page.svelte
+++ src/routes/questionnaire/+page.svelte
@@ -6,57 +6,52 @@
 <h1>Questionnaire</h1>
 
 <form method="post">
-    <fieldset>
-        <legend>Part 1: Programming experience</legend>
+    <h2>Part 1: Programming experience</h2>
 
-        <div class="input-group">
-            <label for="experience">How many years have you been programming?</label>
-            <input
-                type="number"
-                id="experience"
-                name="experience"
-                min="0"
-                max="100"
-                step="1"
-                value="0"
-            />
-        </div>
+    <div class="input-group">
+        <label for="experience">How many years have you been programming?</label>
+        <input
+            type="number"
+            id="experience"
+            name="experience"
+            min="0"
+            max="100"
+            step="1"
+            value="0"
+        />
+    </div>
 
-        <div class="input-group">
-            <label for="python-experience"
-                >How many years have you been programming in Python?</label
-            >
-            <input
-                type="number"
-                id="python-experience"
-                name="python-experience"
-                min="0"
-                max="100"
-                step="1"
-                value="0"
-            />
-        </div>
+    <div class="input-group">
+        <label for="python-experience">How many years have you been programming in Python?</label>
+        <input
+            type="number"
+            id="python-experience"
+            name="python-experience"
+            min="0"
+            max="100"
+            step="1"
+            value="0"
+        />
+    </div>
 
-        <!-- we'll come up with better widgets for likert scales later -->
-        <LikertScale questionId="experience-likert">
-            How would you rate your overall experience with programming?
-        </LikertScale>
+    <!-- we'll come up with better widgets for likert scales later -->
+    <LikertScale questionId="experience-likert">
+        How would you rate your overall experience with programming?
+    </LikertScale>
 
-        <LikertScale questionId="python-familiarity-likert">
-            How would you rate your overall familiarity with Python?
-        </LikertScale>
+    <LikertScale questionId="python-familiarity-likert">
+        How would you rate your overall familiarity with Python?
+    </LikertScale>
 
-        <div class="input-group">
-            <label for="best-language">What programming language are you most experienced in?</label
-            >
-            <input type="text" id="best-language" name="best-language" value="" />
-        </div>
+    <div class="input-group">
+        <label for="best-language">What programming language are you most experienced in?</label>
+        <input type="text" id="best-language" name="best-language" value="" />
+    </div>
 
-        <div class="input-group">
-            <label for="favourite-ide">What is your favourite code editor or IDE?</label>
-            <input type="text" id="favourite-ide" name="favourite-ide" value="" />
-        </div>
-    </fieldset>
+    <div class="input-group">
+        <label for="favourite-ide">What is your favourite code editor or IDE?</label>
+        <input type="text" id="favourite-ide" name="favourite-ide" value="" />
+    </div>
 
     <button type="submit">Submit</button>
 </form>
