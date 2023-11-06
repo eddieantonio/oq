659fc3017bee516778b892859cd6a9f3a3685828
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-06 11:11:52 +0000

    fix: nullable participant in SQLite3

--- migrations/001_init.mjs
+++ migrations/001_init.mjs
@@ -5,12 +5,16 @@
 export const up = async (knex) => {
     return knex.schema
         .createTable('participants', (table) => {
-            table.string('participant_id', 8).primary();
+            table.string('participant_id', 8).primary().notNullable();
             table.timestamp('started_at', { useTz: true }).notNullable();
             table.boolean('consented_to_all').notNullable();
         })
         .createTable('answers', (table) => {
-            table.string('participant_id', 8).references('participant_id').inTable('participants');
+            table
+                .string('participant_id', 8)
+                .references('participant_id')
+                .inTable('participants')
+                .notNullable();
             table.string('question_id', 32);
             table.string('answer', 1024);
 
