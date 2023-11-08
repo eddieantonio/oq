8ce3212e39efb167bbaa546009f2bd0c1ef177c7
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-08 11:21:09 +0000

    change: make debug footer stick at the bottom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,30 @@
 <slot />
 
-<footer>
+<footer class="debug-footer">
     <nav>
         <a href="/_debug">Go to debug page</a>
     </nav>
 </footer>
 
 <style>
-    nav {
+    footer {
         margin-block-start: 2rem;
+    }
+
+    nav {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 1000;
+
+        margin: 0;
+        padding-inline: 1rem;
+        padding-block: 1rem;
+
         font-size: small;
+
+        background-color: var(--nc-bg-3);
+        opacity: 0.5;
     }
 </style>
