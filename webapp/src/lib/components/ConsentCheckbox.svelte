0303f8bde09bf05e83dc62881af53a147dd5aeaa
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-17 11:54:15 +0000

    feat: consent as bulleted list or checkboxes

--- webapp/src/lib/components/ConsentCheckbox.svelte
+++ webapp/src/lib/components/ConsentCheckbox.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
     export let name: string;
-    export let value: boolean;
+    export let value: boolean = true;
+    export let style: 'checkbox' | 'bullet' = 'checkbox';
 </script>
 
-<label>
-    <div class="box">
-        <input type="checkbox" {name} bind:checked={value} />
-    </div>
-    <div class="text">
-        <slot />
-    </div>
-</label>
+<li class:remove-bullets={style == 'checkbox'}>
+    {#if style == 'checkbox'}
+        <label>
+            <div class="box">
+                <input type="checkbox" {name} bind:checked={value} />
+            </div>
+            <div class="text">
+                <slot />
+            </div>
+        </label>
+    {:else}
+        <input type="hidden" {name} value={true} /><slot />
+    {/if}
+</li>
 
 <style>
     label {
@@ -21,6 +28,10 @@
         cursor: pointer;
 
         margin-block-end: 0.5lh;
+    }
+
+    .remove-bullets {
+        list-style: none;
     }
 
     .box {
