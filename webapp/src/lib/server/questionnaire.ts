3e94f975f56afefc382c2f6c9f716bbdeb75ae11
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-11-22 14:00:50 +0000

    refactor: extract function: saveQuestionnaireResponses

---
+++ webapp/src/lib/server/questionnaire.ts
@@ -0,0 +1,35 @@
+import { error, type Cookies } from '@sveltejs/kit';
+import { StatusCodes } from 'http-status-codes';
+
+import { saveAnswers } from '$lib/server/database';
+import type { ParticipantId } from '$lib/server/participants';
+
+/**
+ * Stores the answers to questionnaire in the database.
+ *
+ * At present, this will store ALL entries in the form data as answers.
+ */
+export async function saveQuestionnaireResponses(cookies: Cookies, request: Request) {
+    // Ensure the participant is logged in
+    const participant = cookies.get('participant_id') as ParticipantId;
+    if (!participant) throw error(StatusCodes.BAD_REQUEST, 'No ParticipantId found');
+
+    // Parse the form data
+    const data = await request.formData();
+
+    // Create an Answer record for each field in form data
+    const answers = [];
+    for (const [key, value] of data.entries()) {
+        if (typeof value !== 'string') {
+            throw error(StatusCodes.BAD_REQUEST, 'Cannot upload file as answer');
+        }
+
+        answers.push({
+            participant_id: participant,
+            question_id: key,
+            answer: value
+        });
+    }
+
+    await saveAnswers(answers);
+}
--- webapp/src/routes/(default)/post-questionnaire/+page.server.ts
+++ webapp/src/routes/(default)/post-questionnaire/+page.server.ts
@@ -2,40 +2,16 @@
  * Save post-questionnaire answers to the database.
  */
 
-import { error, redirect } from '@sveltejs/kit';
+import { saveQuestionnaireResponses } from '$lib/server/questionnaire';
+import { redirect } from '@sveltejs/kit';
 import { StatusCodes } from 'http-status-codes';
-
-import { saveAnswers } from '$lib/server/database';
-import type { ParticipantId } from '$lib/server/participants';
-
-// TODO: refactor this so that questionnaire and post-questionnaire share code?
 
 export const actions: import('./$types').Actions = {
     /**
      * POST to save the answers and continue to the next page.
      */
     default: async ({ request, cookies }) => {
-        const participant = cookies.get('participant_id') as ParticipantId;
-        if (!participant) throw error(StatusCodes.BAD_REQUEST, 'No ParticipantId found');
-
-        const data = await request.formData();
-
-        // Create an Answer record for each field in form data
-        const answers = [];
-        for (const [key, value] of data.entries()) {
-            if (typeof value !== 'string') {
-                throw error(StatusCodes.BAD_REQUEST, 'Cannot upload file as answer');
-            }
-
-            answers.push({
-                participant_id: participant,
-                question_id: key,
-                answer: value
-            });
-        }
-
-        await saveAnswers(answers);
-
+        await saveQuestionnaireResponses(cookies, request);
         throw redirect(StatusCodes.SEE_OTHER, '/before-submit');
     }
 };
