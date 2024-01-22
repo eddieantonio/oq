52063ffacafbe526111b3e5f163f213b1ad28485
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-01-22 13:00:24 +0000

    feat: use UUID for participant IDs

--- webapp/src/lib/server/participants.ts
+++ webapp/src/lib/server/participants.ts
@@ -7,8 +7,7 @@ import type { ParticipantId } from './newtypes';
  * Creates a new, unique participant ID.
  */
 export function makeNewParticipantId(): ParticipantId {
-    // For now, there is only one kind of participant ID.
-    return 'TEST01' as ParticipantId;
+    return crypto.randomUUID() as ParticipantId;
 }
 
 /**
