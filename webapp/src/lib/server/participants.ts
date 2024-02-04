0e85eac940f77b60110d321fb32d498f1719ad58
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-04 16:15:16 +0000

    refactor: remove dead code

--- webapp/src/lib/server/participants.ts
+++ webapp/src/lib/server/participants.ts
@@ -1,6 +1,3 @@
-import { StatusCodes } from 'http-status-codes';
-import { error, type Cookies } from '@sveltejs/kit';
-
 import type { ParticipantId } from './newtypes';
 
 /**
@@ -8,16 +5,4 @@ import type { ParticipantId } from './newtypes';
  */
 export function makeNewParticipantId(): ParticipantId {
     return crypto.randomUUID() as ParticipantId;
-}
-
-/**
- * Gets the current participant ID from the participant_id cookie.
- * Throws 401 Unauthorized if the participant is not logged in.
- */
-export function getParticipantIdFromCookies(cookies: Cookies): ParticipantId {
-    // Ensure the participant is logged in
-    const participant = cookies.get('participant_id');
-    if (!participant) throw error(StatusCodes.UNAUTHORIZED, 'No ParticipantId found');
-
-    return participant as ParticipantId;
 }
