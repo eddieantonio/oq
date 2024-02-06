9993c21fb7624c8f74e3e4faf617f090f4360eb5
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-06 17:22:19 +0000

    change: demo Python task

--- webapp/tasks/python-capital-if/main.fixed.py
+++ webapp/tasks/python-capital-if/main.fixed.py
@@ -1,6 +1,6 @@
 def check_password(password, input):
-    If (input == "s3cr37"): print("You are in!")
-    Else: print("Wrong password!")
+    if (input == "s3cr37"): print("You are in!")
+    else: print("Wrong password!")
 
 input = "hunter2"
 check_password("s3cr37", input)
