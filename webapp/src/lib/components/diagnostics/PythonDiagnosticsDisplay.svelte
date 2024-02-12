522619dbd28150120ee45ee6094eb76d74f25e8a
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-12 14:17:58 +0000

    feat: add basic colours to Python tracebacks

--- webapp/src/lib/components/diagnostics/PythonDiagnosticsDisplay.svelte
+++ webapp/src/lib/components/diagnostics/PythonDiagnosticsDisplay.svelte
@@ -9,17 +9,56 @@
     // Prettier does not care about my indenting preferences.
 </script>
 
-<pre><code
-        >{#each error.frames as frame}{'  '}File "{frame.filename}", line {frame.startLineNumber ??
-                '???'}{#if frame.name != null}, in {frame.name}{/if}
-{#if frame.line != null}{'    '}{frame.line}{/if}
-{#if frame.marker != null}{'    '}{frame.marker}{/if}{'\n'}{/each}<span
-            class="exception">{error.exception}</span
+<pre><code class="text"
+        ><span class="traceback-header">Traceback (most recent call last):</span
+        >{'\n'}{#each error.frames as frame}{'  '}File <span class="filename"
+                >"{frame.filename}"</span
+            >, line <span class="number">{frame.startLineNumber ?? '???'}</span
+            >{#if frame.name != null}, in <span class="name">{frame.name}</span
+                >{/if}
+{#if frame.line != null}{'    '}<span class="code">{frame.line}</span
+                >{/if}
+{#if frame.marker != null}{'    '}<span class="squiggles">{frame.marker}</span
+                >{/if}{'\n'}{/each}<span class="exception">{error.exception}</span
         >: {error.message}</code
     ></pre>
 
 <style>
-    .exception {
+    .text {
+        color: #3f7eb4;
+    }
+    .filename,
+    .number,
+    .name {
+        color: #006400;
+    }
+    .exception,
+    .code {
+        color: #8d0908;
+    }
+    .squiggles {
         color: red;
+    }
+
+    .exception {
+        font-weight: bold;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .text {
+            color: #74b3ea;
+        }
+        .filename,
+        .number,
+        .name {
+            color: #46c746;
+        }
+        .code {
+            color: #ffa0b0;
+        }
+        .exception,
+        .squiggles {
+            color: red;
+        }
     }
 </style>
