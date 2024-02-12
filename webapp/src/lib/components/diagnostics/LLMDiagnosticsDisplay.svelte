445a27eb76ff23afda73f33e21763bc6bf8f8599
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-12 15:27:35 +0000

    change: show only exception line of Python error for LLM

--- webapp/src/lib/components/diagnostics/LLMDiagnosticsDisplay.svelte
+++ webapp/src/lib/components/diagnostics/LLMDiagnosticsDisplay.svelte
@@ -5,11 +5,26 @@
     import DiagnosticDisplay from '../DiagnosticDisplay.svelte';
 
     export let diagnostics: LLMEnhancedDiagnostics;
+    $: original = diagnostics.original;
 </script>
 
 <p><strong>Error Message:</strong></p>
 <blockquote>
-    <DiagnosticDisplay diagnostics={diagnostics.original} />
+    {#if original.format == 'parsed-python'}<!-- Show only the last line of a Python traceback -->
+        <pre><code
+                ><span class="exception">{original.diagnostics.exception}</span>: {original
+                    .diagnostics.message}</code
+            ></pre>
+    {:else}
+        <DiagnosticDisplay diagnostics={original} />
+    {/if}
 </blockquote>
 <p><strong>Explanation:</strong></p>
 {@html marked.parse(diagnostics.markdown)}
+
+<style>
+    .exception {
+        color: red;
+        font-weight: bold;
+    }
+</style>
