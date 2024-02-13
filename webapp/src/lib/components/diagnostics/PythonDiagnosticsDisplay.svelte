fbc4a01cc830a29f77a86175111cb565ca092e32
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-13 11:15:44 +0000

    feat: steal Jupyter's colors for tracebacks

--- webapp/src/lib/components/diagnostics/PythonDiagnosticsDisplay.svelte
+++ webapp/src/lib/components/diagnostics/PythonDiagnosticsDisplay.svelte
@@ -20,11 +20,11 @@
 <pre><code class="text"
         >{#if error.hasTraceback}<span class="traceback-header"
                 >Traceback (most recent call last):</span
-            >{'\n'}{/if}{#each error.frames as frame}{'  '}File <span class="filename"
-                >"{filenameOnly(frame.filename)}"</span
-            >, line <span class="number">{frame.startLineNumber ?? '???'}</span
-            >{#if frame.name != null}, in <span class="name">{frame.name}</span
-                >{/if}
+            >{'\n'}{/if}{#each error.frames as frame}{'  '}<span class="frame-header"
+                >File <span class="filename">"{filenameOnly(frame.filename)}"</span>, line <span
+                    class="line-number">{frame.startLineNumber ?? '???'}</span
+                >{#if frame.name != null}, in <span class="name">{frame.name}</span>{/if}</span
+            >
 {#if frame.line != null}{'    '}<span class="code">{frame.line}</span
                 >{/if}
 {#if frame.marker != null}{'    '}<span class="squiggles">{frame.marker}</span
@@ -33,41 +33,31 @@
     ></pre>
 
 <style>
-    .text {
-        color: #3f7eb4;
+    /* Styles that are sort of inpsired by IPython/Jupyter's colours. */
+    /* Surprisingly, they work both in dark and light modes. */
+    :root {
+        --ansi-cyan-fg: #60c6c8;
+        --ansi-green-fg: #00a250;
+        --ansi-red-fg: #e75c58;
     }
+
+    .frame-header {
+        color: var(--ansi-cyan-fg);
+    }
+
     .filename,
-    .number,
+    .line-number,
     .name {
-        color: #006400;
+        color: var(--ansi-green-fg);
     }
-    .exception,
-    .code {
-        color: #8d0908;
-    }
-    .squiggles {
-        color: red;
+
+    .squiggles,
+    .exception {
+        color: var(--ansi-red-fg);
     }
 
     .exception {
+        /* This isn't part of IPython/Jupyter's colours, but it SHOULD BE! */
         font-weight: bold;
-    }
-
-    @media (prefers-color-scheme: dark) {
-        .text {
-            color: #74b3ea;
-        }
-        .filename,
-        .number,
-        .name {
-            color: #46c746;
-        }
-        .code {
-            color: #ffa0b0;
-        }
-        .exception,
-        .squiggles {
-            color: red;
-        }
     }
 </style>
