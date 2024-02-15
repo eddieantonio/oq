65861916902a1c4168f351cb8185955822cd3b9b
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-15 11:18:00 +0000

    feat: render ANSI colours for Rust diagnostics

--- webapp/src/lib/components/diagnostics/RustDiagnosticsDisplay.svelte
+++ webapp/src/lib/components/diagnostics/RustDiagnosticsDisplay.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import type { RustDiagnostics } from '$lib/types/diagnostics';
+    import { AnsiUp } from 'ansi_up';
 
     export let diagnostics: RustDiagnostics;
+    const ansi_up = new AnsiUp();
 </script>
 
 {#each diagnostics.diagnostics as diagnostic}
-    <pre><code>{diagnostic.rendered}</code></pre>
+    {#if diagnostic.rendered != null}
+        <pre><code>{@html ansi_up.ansi_to_html(diagnostic.rendered)}</code></pre>
+    {:else}
+        <pre><code>{@html ansi_up.ansi_to_html(diagnostic.message)}</code></pre>
+    {/if}
 {/each}
