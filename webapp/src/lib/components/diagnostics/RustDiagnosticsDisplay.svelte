4b8ba071a3f0e6e9dd2b3535b0c3ac9f4a9d790a
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-02-20 12:06:52 +0000

    change: show only first line of Rust output in LLM display

--- webapp/src/lib/components/diagnostics/RustDiagnosticsDisplay.svelte
+++ webapp/src/lib/components/diagnostics/RustDiagnosticsDisplay.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
-    import type { RustDiagnostics } from '$lib/types/diagnostics';
+    import type { RustDiagnostic, RustDiagnostics } from '$lib/types/diagnostics';
     import { AnsiUp } from 'ansi_up';
 
     export let diagnostics: RustDiagnostics;
+    export let showDiagnosticWindow: boolean = true;
     const ansi_up = new AnsiUp();
+
+    function getMessage(diagnostic: RustDiagnostic): string {
+        const output = diagnostic.rendered ?? diagnostic.message;
+        return showDiagnosticWindow ? output : firstLine(output);
+    }
+
+    function firstLine(message: string): string {
+        return message.split('\n')[0];
+    }
 </script>
 
 {#each diagnostics.diagnostics as diagnostic}
-    {#if diagnostic.rendered != null}
-        <pre><code>{@html ansi_up.ansi_to_html(diagnostic.rendered)}</code></pre>
-    {:else}
-        <pre><code>{@html ansi_up.ansi_to_html(diagnostic.message)}</code></pre>
-    {/if}
+    <pre><code>{@html ansi_up.ansi_to_html(getMessage(diagnostic))}</code></pre>
 {/each}
